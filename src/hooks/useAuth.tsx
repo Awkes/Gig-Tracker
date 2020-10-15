3372047c8576cb10a2fd6f5a4e049fa775a4fcd9
Andreas Åkerlöf <a.akerlof@gmail.com>
2020-10-15 15:38:09 +0200

    refactor(auth): moved signin/signout functions from AuthContext to useAuth hook

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,10 +1,32 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 import AuthContext from "../contexts/AuthContext"
 
+const tempUser = 'user@host.se';
+const tempPass = 'password';
+
 const useAuth = () => {
-  const context = useContext<any>(AuthContext);
-  return context;
+  const { authUser, setAuthUser } = useContext<any>(AuthContext);
+  const [signInError, setSignInError] = useState<string|null>(null);
+
+  function signIn(email: string|undefined, password: string|undefined) {
+    if (email?.toLowerCase() === tempUser && password === tempPass) {
+      sessionStorage.setItem('authUser', email.toLowerCase());
+      setAuthUser(email.toLowerCase());
+      setSignInError(null);
+    }
+    else {
+      setAuthUser(null);
+      setSignInError('Invalid credentials.')
+    }
+  }
+
+  function signOut() {
+    sessionStorage.removeItem('authUser');
+    setAuthUser(null);
+  }
+
+  return { authUser, signIn, signInError, signOut };
 }
 
 export default useAuth;
