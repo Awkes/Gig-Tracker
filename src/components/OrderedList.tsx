4aec6f516cc938a02ebac4cb085714eeced88989
Andreas Åkerlöf <a.akerlof@gmail.com>
2020-10-22 14:17:59 +0200

    style(orderedlist): removed margin

--- src/components/OrderedList.tsx
+++ src/components/OrderedList.tsx
@@ -7,7 +7,7 @@ type Props = {
 }
 
 const OrderedList = ({ values }: Props) => (
-  <ol>
+  <ol sx={{ margin: 0 }}>
     {values.map((value, i) => (
       <li key={`list-item-${i}`}>
         {value}
