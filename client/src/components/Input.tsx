b74bc26796748f93b088703307ef29981e89ce84
Andreas Åkerlöf <a.akerlof@gmail.com>
2020-12-02 12:04:49 +0100

    feat(input): added error prop

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,20 +1,25 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 
-const Input = (props: any) => (
-  <input 
-    sx={{ 
-      width: '100%',
-      padding: 1,
-      fontFamily: 'text',
-      fontSize: 2,
-      letterSpacing: 1,
-      border: '1px solid',
-      borderColor: 'border',
-      borderRadius: 0,
-    }} 
-    {...props}
-  />
-);
+const Input = (props: any) => {
+  const { error } = props;
+  return (
+    <input 
+      sx={{ 
+        width: '100%',
+        padding: 1,
+        fontFamily: 'text',
+        fontSize: 2,
+        color: 'text',
+        letterSpacing: 1,
+        border: '1px solid',
+        borderColor: 'border',
+        borderRadius: 0,
+        backgroundColor: error ? 'errorBg' : 'background'
+      }} 
+      {...props}
+    />
+  );
+}
 
 export default Input;
